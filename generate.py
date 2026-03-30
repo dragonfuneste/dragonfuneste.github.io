3a6283e5c6b28e11bc20462242aadb68e260b0b9
dragonfuneste <128384397+dragonfuneste@users.noreply.github.com>
2026-03-30 14:38:22 +0200

    Update site content

--- generate.py
+++ generate.py
@@ -17,13 +17,8 @@ def main():
     print(f"Clés trouvées dans le YAML : {data.keys()}") # Vérifie si 'education' est ici
     # 1. Générer le fichier .tex pour le CV Académique
     template_tex = latex_env.get_template('academic.tex')
-    with open('cv_academic.tex', 'w', encoding='utf-8') as f:
+    with open('academic.tex', 'w', encoding='utf-8') as f:
         f.write(template_tex.render(data))
-
-    # 2. Générer le fichier .html pour le CV Ingénierie
-    template_html = html_env.get_template('engineering.html')
-    with open('cv_engineering.html', 'w', encoding='utf-8') as f:
-        f.write(template_html.render(data))
 
 if __name__ == "__main__":
     main()
